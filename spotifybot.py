b65ef946d30c56adb759ca05b66f1848af16cfba
mr-aydemir <aydemir_emre65@hotmail.com>
2022-08-02 21:08:52 +0300

    Update spotifybot.py

--- spotifybot.py
+++ spotifybot.py
@@ -14,10 +14,14 @@ if __name__ == "__main__":
     username= "tarimtanyeri@gmail.com"
     password= "Abrakadabra12"
     # Bot aşamaları
+    
+    # Giriş Yap
     bot.open_url("https://accounts.spotify.com/tr/login") # spotify login aç
     bot.set_input_and_next("#login-username", username) # username gir ve bir sonrakine ilerle
     bot.set_input_and_next("#login-password", password) # password gir ve bir sonrakine ilerle
     bot.wait_and_click_element("button[data-testid='web-player-link']") # web player butonunun oluşmasını bekle ve tıkla
+
+    # Müzik Listesini aç
     bot.wait_and_click_element(".GlueDropTarget--playlists.GlueDropTarget--folders") # oluşturmuş olunan ilk listenin oluşmasını bekle ve tıkla
     bot.wait_and_click_element("div[data-testid='action-bar-row'] button[data-testid='play-button']") # play butonun gelmesini bekle ve tıkla
     time.sleep(1000) # 1000 saniye bekle tarayıcının kapanmaması için
