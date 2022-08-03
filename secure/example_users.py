d5510f1949f785a8bf5fe652fe9f0d1ea13ff4ef
mr-aydemir <aydemir_emre65@hotmail.com>
2022-08-03 19:02:11 +0300

    Update example_users.py

--- secure/example_users.py
+++ secure/example_users.py
@@ -1,4 +1,6 @@
-# users.py dosyasının içine bu aşağıdaki listeyi koyun
+#Bu dosya users.py dosyasının oluşturulmasına örnek olarak yazılmıştır.
+#bu dosyanın bulunduğu klasörde users.py dosyası oluşturun ve bu users.py dosyasının içine bu aşağıdaki listeyi koyun
+# - secure/users.py
 from data.user import User
 users = [User(name="abc@gmail.com", password="aaaa"),
     User(name="acd@hotmail.com", password="dddd")]
